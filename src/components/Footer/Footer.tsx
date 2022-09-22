c89bbd91a66789a925b00f53481596325e179165
VV063115 <Vlad.Vira@cerner.com>
2022-09-22 15:29:45 +0300

    ProjectStructure

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,31 @@
+import Stack from "@mui/material/Stack";
+import IconButton from "@mui/material/IconButton";
+import Link from "@mui/material/Link";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import Typography from "@mui/material/Typography";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className={styles.footer}>
+      <Typography>@{year} PhotoGallery</Typography>
+      <Stack direction="row" justifyContent="center">
+        <IconButton color="secondary">
+          <Link href={"https://www.facebook.com/"}>
+            <FacebookIcon />
+          </Link>
+        </IconButton>
+        <IconButton color="secondary">
+          <Link href="https://www.instagram.com/">
+            <InstagramIcon />
+          </Link>
+        </IconButton>
+      </Stack>
+    </footer>
+  );
+};
+
+export default Footer;
